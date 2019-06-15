84d2f588a1e3137e3868e2783b8a62037fcda1e5
alan8656 <alan8656123@gmail.com>
2019-06-16 04:03:31 +0800

    start activity OK

--- RoomWordsWithDelete/app/src/main/java/com/android/example/roomwordssample/StartActivity.java
+++ RoomWordsWithDelete/app/src/main/java/com/android/example/roomwordssample/StartActivity.java
@@ -1,9 +1,19 @@
 package com.android.example.roomwordssample;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import static com.android.example.roomwordssample.MainActivity.NEW_WORD_ACTIVITY_REQUEST_CODE;
 
@@ -12,13 +22,112 @@ import com.android.example.roomwordssample.R;
 import com.android.example.roomwordssample.Word;
 import com.android.example.roomwordssample.WordListAdapter;
 import com.android.example.roomwordssample.WordViewModel;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class StartActivity extends AppCompatActivity {
     public static final int START_ACTIVITY_REQUEST_CODE = 100;
+
+
+    private int time=0;
+    private boolean first_clicked=true;
+    private ImageView title_image;
+    private ImageView que_image;
+    private RadioButton R_teacher;
+    private RadioButton R_student;
+    private RadioGroup tea_or_std_group;
+    private ImageView tea_image;
+    private ImageView std_image;
+    private TextView class_text;
+    private TextView nickname_text;
+    private EditText nick_edittext;
+    private EditText class_edittext;
+    private Activity this_act;
+    private Button start_btn;
+    Timer timer = new Timer(true);
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        this_act=this;
+        timer.schedule(new MyTimerTask(), 2000, 1000);
+
+        title_image=findViewById(R.id.title_image);
+        que_image=findViewById(R.id.que_imageview);
+        R_teacher=findViewById(R.id.radio_btn_teacher);
+        R_student=findViewById(R.id.radio_btn_student);
+        tea_or_std_group=findViewById(R.id.tea_std_group);
+        tea_image=findViewById(R.id.teacher_img);
+        std_image=findViewById(R.id.student_img);
+        class_text=findViewById(R.id.class_textview);
+        class_edittext=findViewById(R.id.class_editText);
+        nickname_text=findViewById(R.id.nicktextView);
+        nick_edittext=findViewById(R.id.nickname_editText);
+        start_btn=findViewById(R.id.start_btn);
+
+
+        Animation start_ami=AnimationUtils.loadAnimation(this_act,R.anim.start_anim);
+        title_image.setAnimation(start_ami);
+        Animation que_start_ami=AnimationUtils.loadAnimation(this_act,R.anim.question_rotate);
+        que_image.setAnimation(que_start_ami);
+        Animation boo2 =AnimationUtils.loadAnimation(this_act,R.anim.choose_anim);
+        R_student.setAnimation(boo2);
+        R_teacher.setAnimation(boo2);
+
+        nickname_text.setVisibility(View.INVISIBLE);
+        nick_edittext.setVisibility(View.INVISIBLE);
+        class_text.setVisibility(View.INVISIBLE);
+        class_edittext.setVisibility(View.INVISIBLE);
+
+        tea_image.setVisibility(View.INVISIBLE);
+        std_image.setVisibility(View.INVISIBLE);
+        start_btn.setVisibility(View.INVISIBLE);
+        tea_or_std_group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                /*class_text.setVisibility(View.VISIBLE);
+                class_edittext.setVisibility(View.VISIBLE);*/
+                Animation fadein =AnimationUtils.loadAnimation(this_act,R.anim.fadein);
+                Animation fadeout =AnimationUtils.loadAnimation(this_act,R.anim.fadeout);
+
+                if(first_clicked) {
+                    class_text.setAnimation(fadein);
+                    class_edittext.setAnimation(fadein);
+                    start_btn.setAnimation(fadein);
+                }
+
+                switch (checkedId){
+                    case R.id.radio_btn_teacher:
+                        tea_image.setAnimation(fadein);
+                        if(!first_clicked) {
+                            std_image.setAnimation(fadeout);
+
+                            nickname_text.setAnimation(fadeout);
+                            nick_edittext.setAnimation(fadeout);
+                            class_text.setText("創建新課");
+                        }
+                        break;
+                    case R.id.radio_btn_student:
+                        if(!first_clicked)tea_image.setAnimation(fadeout);
+                        std_image.setAnimation(fadein);
+
+                        nickname_text.setAnimation(fadein);
+                        nick_edittext.setAnimation(fadein);
+                        class_text.setText(R.string.class_name);
+                        break;
+
+                }
+
+                if(first_clicked)first_clicked=false;
+            }
+        });
+
 
 
     }
@@ -28,4 +137,19 @@ public class StartActivity extends AppCompatActivity {
         startActivityForResult(intent, START_ACTIVITY_REQUEST_CODE);
 
     }
+    public class MyTimerTask extends TimerTask
+    {
+        public void run()
+        {
+            time++;
+            Log.d("cccc","time"+time);
+            if(time==0){
+
+
+            } else if (time == 5) {
+
+            }
+        }
+    }
+
 }
